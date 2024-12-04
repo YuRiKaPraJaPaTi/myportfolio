017d7abef2fe948f587a2fbac5e55f079f437330
YURIKA PRAJAPATI <prajapatiyurika@gmail.com>
2024-12-04 07:35:16 +0545

    first

---
+++ src/assets/index.js
@@ -0,0 +1,18 @@
+import logo from "./images/logo.png";
+import banner from "./images/banner.jpg";
+
+import project1 from "./images/projects/project1.png";
+import project2 from "./images/projects/project2.png";
+import project3 from "./images/projects/project3.jpg";
+import project4 from "./images/projects/project4.png";
+import contact from "./images/contact/contact.png";
+
+export {
+  logo,
+  banner,
+  project1,
+  project2,
+  project3,
+  project4,
+  contact,
+};
